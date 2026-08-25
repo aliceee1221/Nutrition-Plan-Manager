a406fa2094742a736a106c67399582edc89b9b58
Alice Pei <n12697532@qut.edu.au>
2026-08-26 02:54:29 +1000

    NPM-5 Adapt user model for authentication and roles

--- backend/models/User.js
+++ backend/models/User.js
@@ -1,4 +1,3 @@
-
 const mongoose = require('mongoose');
 const bcrypt = require('bcrypt');
 
@@ -6,8 +5,9 @@ const userSchema = new mongoose.Schema({
     name: { type: String, required: true },
     email: { type: String, required: true, unique: true },
     password: { type: String, required: true },
-    university: { type: String },
-    address: { type: String },
+    role: { type: String, required: true, enum: ['client', 'nutritionist'] }
+}, {
+    timestamps: true
 });
 
 userSchema.pre('save', async function (next) {
